90cb45f86f96d4c43268c703baa4c96ab61b1f92
MTtheman <96625711+MTtheman@users.noreply.github.com>
2022-02-22 16:01:44 -0500

    Changed tempo of 2nd beat to 50.

--- main.ts
+++ main.ts
@@ -4,5 +4,5 @@ forever(function () {
     music.playMelody("F D B E G B G D ", 263)
 })
 forever(function () {
-    music.playMelody("C - E - C - E - ", 120)
+    music.playMelody("C - E - C - E - ", 50)
 })
