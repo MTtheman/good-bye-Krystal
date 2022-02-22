db3a8f12fadeba30eb43b1aa64251fa28fa59fbc
MTtheman <96625711+MTtheman@users.noreply.github.com>
2022-02-22 15:43:53 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,8 @@
+scene.setBackgroundImage(assets.image`Backround Goodbye Krystal`)
+music.beamUp.playUntilDone()
+forever(function () {
+    music.playMelody("F D B E G B G D ", 263)
+})
+forever(function () {
+    music.playMelody("C - E - C - E - ", 120)
+})
