44ddff32019247ad9d938cbd946faba7084082ef
MTtheman <96625711+MTtheman@users.noreply.github.com>
2022-02-23 09:28:59 -0500

    Changing from music forever to ten times repeated.

--- main.ts
+++ main.ts
@@ -1,8 +1,2 @@
 scene.setBackgroundImage(assets.image`Backround Goodbye Krystal`)
 music.beamUp.playUntilDone()
-forever(function () {
-    music.playMelody("F D B E G B G D ", 263)
-})
-forever(function () {
-    music.playMelody("C - E - C - E - ", 50)
-})
