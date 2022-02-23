b9ab87bc953d9befc6c8360e6f96a4c9890e8d61
MTtheman <96625711+MTtheman@users.noreply.github.com>
2022-02-23 09:30:55 -0500

    Fixed placing of music blocks so they can play.

--- main.ts
+++ main.ts
@@ -1,2 +1,8 @@
 scene.setBackgroundImage(assets.image`Backround Goodbye Krystal`)
 music.beamUp.playUntilDone()
+for (let index = 0; index < 10; index++) {
+    music.playMelody("F D B E G B G D ", 263)
+}
+for (let index = 0; index < 10; index++) {
+    music.playMelody("C - E - C - E - ", 50)
+}
